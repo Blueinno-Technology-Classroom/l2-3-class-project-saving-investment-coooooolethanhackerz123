12001cdd139c0c779bfca51c2bf0c08e24cba406
cooooooolethanhackerz22113 <wangethanhk@gmail.com>
2024-02-03 11:26:22 +0800

    a

--- main.py
+++ main.py
@@ -4,7 +4,19 @@ Q1:
 '''
 
 # TODO: Write your code here
+current_balance = 0
+interest_rate= 0.065
+interest = 0
+deposit = 100
+new_balance = 1000
 
+for i in range(25):
+    current_balance = new_balance
+    interest = interest_rate * new_balance 
+    new_balance = current_balance + interest
+    print(f'{i+1:02}:current_balance:{current_balance:.2f}, interest:{interest},deposit:{deposit}, new_balance:{new_balance:.2f}')
+    if i > 0:
+        new_balance += deposit
 ##################################################
 '''
 Q2:
